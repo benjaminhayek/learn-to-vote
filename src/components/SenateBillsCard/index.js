71046e43426448f98885ef7e1eb6ca55e38ebe7d
bhayek12@gmail.com <bhayek12@gmail.com>
2018-11-03 15:53:31 -0600

    Add some style to bill card

--- src/components/SenateBillsCard/index.js
+++ src/components/SenateBillsCard/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './SenateBillsCard.css';
 
 export class SenateBillsCard extends Component {
     constructor() {
@@ -8,10 +9,10 @@ export class SenateBillsCard extends Component {
     render() {
         const { bills } = this.props       
         return(
-            <div>
-                <h1 className='title'>{bills.committees}</h1>
-                <h2 className='title'>{bills.title}</h2>
-                <h2 className='party'>{bills.api_uri}</h2>
+            <div className='bills-container'>
+                <h1 className='committee'>Committee: {bills.committee}</h1>
+                <h2 className='bill-title'>Title: {bills.title}</h2>
+                <h2 className='url'>Click here for more info: {bills.url}</h2>
             </div>
         )
     }
