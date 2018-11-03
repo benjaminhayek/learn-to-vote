dcc0aa489b2fdbc5e38a443d47e70b14aa08c28a
bhayek12@gmail.com <bhayek12@gmail.com>
2018-11-03 16:15:04 -0600

    Create member bills card

---
+++ src/components/MemberBillsCard/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './SenateBillsCard.css';
+
+export class MemberBillsCard extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        const { bills } = this.props       
+        return(
+            <div className='bills-container'>
+                <h1 className='committee'>Committee: {bills.committee}</h1>
+                <h2 className='bill-title'>Title: {bills.title}</h2>
+                <a className='url' href={bills.url}>Click here for more info</a>
+            </div>
+        )
+    }
+}
+
+export default MemberBillsCard
