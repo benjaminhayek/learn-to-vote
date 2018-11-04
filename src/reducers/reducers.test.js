30746b250895976f474224ba0173a5d48920e3bf
bhayek12@gmail.com <bhayek12@gmail.com>
2018-11-03 19:14:16 -0600

    Add tests for congressmen reducer

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -20,3 +20,26 @@ describe('billsReducer', () => {
         expect(result).toEqual(expected);
       })
 })
+
+describe('congressmenReducer', () => {
+    it('should return the initial state', () => {
+        const expected = [];
+        const result = congressmenReducer(undefined, {});
+        expect(result).toEqual(expected);
+      });
+    
+      it('should update state with bills when addCongressmen is dispatched', () => {
+        const initialState = [];
+        const congressmen = [{name: 'Ted', id: 1, party: 'D', title: 'Representative', nextElection: '2018', selected: false}, {name: 'Jan', id: 2, party: 'R', title: 'Representative', nextElection: '2018', selected: false}];
+        const expected = [...congressmen];
+        const result = congressmenReducer(initialState, actions.addCongressmen(congressmen));
+        expect(result).toEqual(expected);
+      })
+
+      it('should toggle selected when toggle selected is dispatched', () => {
+        const initialState = [];
+        const expected = [];
+        const result = congressmenReducer(initialState, actions.toggleSelected({id:1}))
+        expect(result).toEqual(expected)
+      })
+})
