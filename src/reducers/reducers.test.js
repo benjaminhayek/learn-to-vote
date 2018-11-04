4c73942d0d094caa38413afe397e8354faddcac6
bhayek12@gmail.com <bhayek12@gmail.com>
2018-11-03 19:18:05 -0600

    Add tests for loading reducer

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -66,3 +66,19 @@ describe('senatorReducer', () => {
         expect(result).toEqual(expected)
       })
 })
+
+describe('loadingReducer', () => {
+    it('should return the initialState', () => {
+      const expected = '';
+      const result = loadingReducer(undefined, '');
+      expect(result).toEqual(expected);
+    })
+  
+    it('should update with a new status when loadingReducer is dispatched', () => {
+      const initialState = '';
+      const status = 'loading';
+      const expected = 'loading';
+      const result = loadingReducer(initialState, actions.contentStatus(status));
+      expect(result).toEqual(expected);
+    });
+  })
