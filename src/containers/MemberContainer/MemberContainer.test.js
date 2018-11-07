c2356113c1def0f5538245384d969392d7cae80d
bhayek12@gmail.com <bhayek12@gmail.com>
2018-11-06 18:46:35 -0700

    Lint

--- src/containers/MemberContainer/MemberContainer.test.js
+++ src/containers/MemberContainer/MemberContainer.test.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { MemberContainer } from './index';
 import { mapStateToProps, mapDispatchToProps } from './index.js';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 describe('MemberContainer', () => {
   let wrapper;
   let congressmen;
-  let clearSelected;
   
   beforeEach(() => {
-    clearSelected = jest.fn();
     congressmen = [{name: 'Ted', id: 1, party: 'D', title: 'Representative', nextElection: '2018', selected: false}, {name: 'Jan', id: 2, party: 'R', title: 'Representative', nextElection: '2018', selected: false}];    
     wrapper = shallow(<MemberContainer congressmen={congressmen} />);
   })
